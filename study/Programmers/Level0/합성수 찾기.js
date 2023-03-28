3a3e4cb857da906c1fb08234284c92846c39f664
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-03-29 03:22:52 +0900

    프로그래머스 Level0 70문 -오답 수정 전- 완

--- study/Programmers/Level0/합성수 찾기.js
+++ study/Programmers/Level0/합성수 찾기.js
@@ -4,3 +4,20 @@
 //결과 
 //정확성: 100.0
 //합계: 100.0 / 100.0
+
+function solution(n) {
+    var answer = 0;
+    var cnt;
+    
+    for(let i = 4; i <= n; i++) {
+        cnt = 1;
+        for(let j = 2; j <= i; j++) {
+            if (i % j == 0)
+                cnt ++;
+        }
+        
+        if (cnt >= 3) answer ++;
+    }
+    
+    return answer;
+}
