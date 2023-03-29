4d9620ec06d5937ff6600c26a4fc10dd1bca9429
2UJIN <83401978+2UJ1N@users.noreply.github.com>
2023-03-29 16:41:38 +0900

    프로그래머스 level0 -ing

--- study/Programmers/Level0/구슬을 나누는 경우의 수.js
+++ study/Programmers/Level0/구슬을 나누는 경우의 수.js
@@ -1,6 +1,20 @@
-//[level 0] 구슬을 나누는 경우의 수
-//https://school.programmers.co.kr/learn/courses/30/lessons/120842
+//[level 0] 구슬을 나누는 경우의 수 120840
+//https://school.programmers.co.kr/learn/courses/30/lessons/120840
 
 //결과 
-//정확성: 100.0
-//합계: 100.0 / 100.0
+//정확성: 80.0
+//합계: 80.0 / 100.0
+
+function solution(balls, share) {
+    
+    return factorial(balls) / factorial(balls - share) / factorial(share);
+}
+
+function factorial(num) {
+    answer = 1;
+    
+    for (let i = num; i > 0; i--) {
+        answer *= i;
+    }
+    return answer;
+}
